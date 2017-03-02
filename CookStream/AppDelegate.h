9b806cc6c216351e08b1ee318476a47b8576664a
romanvintish <romanvintish@gmail.com>
2017-03-02 16:55:59 +0200

    Initial Commit

---
+++ CookStream/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CookStream
+//
+//  Created by VINTISH ROMAN on 3/2/17.
+//  Copyright © 2017 Roman Vintish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CookStream/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CookStream
+//
+//  Created by VINTISH ROMAN on 3/2/17.
+//  Copyright © 2017 Roman Vintish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
